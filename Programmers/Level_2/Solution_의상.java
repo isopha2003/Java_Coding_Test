cd2c9fec474d89ebbf7204cde82bf3a97876db58
isopha2003@gmail.com <isopha2003@gmail.com>
2026-04-06 11:13:58 +0900

    feat: [Programmers] 의상 (Level 2)

---
+++ Programmers/Level_2/Solution_의상.java
@@ -0,0 +1,17 @@
+import java.util.Map;
+import java.util.HashMap;
+
+class Solution {
+    public int solution(String[][] clothes) {
+        int answer = 1;
+        Map<String, Integer> closet = new HashMap<>();
+        for (int i = 0 ; i < clothes.length; i++) {
+            closet.put(clothes[i][1], closet.getOrDefault(clothes[i][1], 0) + 1);
+        }
+        for (Map.Entry<String, Integer> entry : closet.entrySet()) {
+            answer *= entry.getValue() + 1;
+        }
+        
+        return answer - 1;
+    }
+}
